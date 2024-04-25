2d91bd967429c62612ddead23b8da5a45035a0e1
Morabet <ismailelmorabet2@gmail.com>
2024-04-25 13:45:10 +0000

    fixed 100

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -27,7 +27,7 @@ BEGIN
     UPDATE users SET average_score = average WHERE id = user_id;
     
     -- Return the computed weighted score
-    SELECT weighted_score AS Message;
+    -- SELECT weighted_score AS Message;
 END //
 
 DELIMITER ;
