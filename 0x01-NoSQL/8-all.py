cc319a5338bbbb76294cdc47cf66c2c21683d10c
Morabet <ismailelmorabet2@gmail.com>
2024-04-29 11:24:02 -0700

    mandatory tasks

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Task 8"""
+
+from typing import List
+
+
+def list_all(mongo_collection) -> List:
+    """  lists all documents in a collection:"""
+
+    return mongo_collection.find()
