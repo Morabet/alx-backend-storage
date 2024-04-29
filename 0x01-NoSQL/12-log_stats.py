c261629b703ff032e354772bb939a6087c9d3886
Morabet <ismailelmorabet2@gmail.com>
2024-04-29 11:39:39 -0700

    fixing task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,16 +3,29 @@
 
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-nginx_collection = client.logs.nginx
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+def print_nginx_request_logs(nginx_collection):
+    '''Prints stats about Nginx request logs.
+    '''
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-print("{} logs".format(nginx_collection.count_documents({})))
-print("Methods:")
-for method in methods:
-    print("\t method {}: {} \
-    " .format(method, nginx_collection.count_documents({'method': method})))
+    print("{} logs".format(nginx_collection.count_documents({})))
+    print("Methods:")
+    for method in methods:
+        req_count = nginx_collection.count_documents({'method': method})
+        print("\t method {}: {} \
+        " .format(method, req_count))
 
-print("{} status check \
-    ".format(nginx_collection.count_documents({'path': '/status'})))
+    print("{} status check \
+        ".format(nginx_collection.count_documents({'path': '/status'})))
+
+
+def run():
+    '''Provides some stats about Nginx logs stored in MongoDB.
+    '''
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    print_nginx_request_logs(client.logs.nginx)
+
+
+if __name__ == '__main__':
+    run()
